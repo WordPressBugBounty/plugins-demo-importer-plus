6175389acebc690d1b4d69328903f6273ca05687
Sean Murphy <sean@iamseanmurphy.com>
2024-09-22 19:15:20 +0000

    2.0.0

---
+++ demo-importer-plus/dist/admin.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0b6a332169438c7eb4af');
---
+++ demo-importer-plus/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kraftplugins/demo-importer-plus',
+        'pretty_version' => 'dev-next',
+        'version' => 'dev-next',
+        'reference' => '7fcc0d30fe10f90de7b6da528aa8e13591ccb30e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kraftplugins/demo-importer-plus' => array(
+            'pretty_version' => 'dev-next',
+            'version' => 'dev-next',
+            'reference' => '7fcc0d30fe10f90de7b6da528aa8e13591ccb30e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
